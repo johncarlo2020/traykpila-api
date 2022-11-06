b1c7836ed1de9cffedb9b305fb4e1dcc280097d6
johncarlo2020 <77434763+johncarlo2020@users.noreply.github.com>
2022-11-06 16:25:45 +0800

    add address

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,7 +14,7 @@ class AuthController extends Controller
 
         $attrs= $request->validate([
             'name'=>'required|string',
-            'address'=>'required'|'String',
+            'address'=>'String',
             'email'=>'required|email|unique:users,email',
             'password'=>'required|min:6|confirmed',
             'role' => 'required'
