914ed88dcc5057cccb19ef603492d2bcfcaea299
vrixslang <104350366+vrixslang@users.noreply.github.com>
2022-12-09 15:58:10 +0800

    vrixschanges

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,225 @@
-fsdfsdasdasdas
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TraykPila</title>
+    <link rel="stylesheet" href="css/home.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light shadow-sm bg-light fixed-top">
+    <div class="container"> <a class="navbar-brand d-flex align-items-center" href="#">
+    <img src="images/traykpilalogo.png" width="200px" version="1.1" alt="">
+    <g>
+    <g>
+    <path d="M511.981,118.509c-0.135-2.956-1.892-5.726-4.565-7.04l-159.24-79.62c-3.776-1.887-8.363-0.64-10.664,2.898 L229.916,200.282l-55.413-85.891c-0.796-1.235-1.945-2.264-3.259-2.922l-159.24-79.62C6.582,29.138,0,33.218,0,39.268v79.621 c0,4.581,3.712,8.294,8.294,8.294c4.581,0,8.294-3.712,8.294-.294V52.687l142.652,71.326v335.32L16.587,388.008V154.277	c0-4.581-3.712-8.294-8.294-8.294c-4.581,0-8.294,3.712-8.294,8.294v238.857c0,3.142,1.775,6.013,4.585,7.418l159.24,79.62	c5.427,2.714,12.003-1.375,12.003-7.418V318.087l324.17,162.085c5.427,2.714,12.003-1.375,12.003-7.418V118.888	C512,118.76,511.987,118.636,511.981,118.509z M175.827,299.541v-152.5l145.239,225.12L175.827,299.541z M495.413,459.335
+    l-139.34-69.671l108.576-186.993c2.299-3.961,0.952-9.037-3.009-11.337c-3.96-2.298-9.036-0.953-11.337,3.009L344.094,377.258
+    L239.77,215.555L347.383,49.998l144.717,72.359l-26.387,45.446c-2.299,3.961-0.952,9.037,3.009,11.337	c3.958,2.297,9.035,0.953,11.337-3.009l15.354-26.443V459.335z" fill="#000" />
+    </g>
+    </g>
+    </svg>
+    <!-- <span class="ml-3 font-weight-bold">BRAND</apan> -->
+    </a> <button class="navbar-toggler navbar-toggler-right border-0" type="button" data-toggle="collapse" data-target="#navbar4">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+
+
+    <div class="collapse navbar-collapse" id="navbar4">
+    <ul class="navbar-nav mr-auto pl-lg-4">
+        <li class="nav-item px-lg-2 active"> <a class="nav-link" href="#"> <span class="d-inline-block d-lg-none icon-width"><i class="fas fa-home"></i></span>Home</a> </li>
+        <li class="nav-item px-lg-2"> <a class="nav-link" href="#"><span class="d-inline-block d-lg-none icon-width"><i class="fas fa-spa"></i></span>Services</a> </li>
+        <li class="nav-item px-lg-2"> <a class="nav-link" href="#"><span class="d-inline-block d-lg-none icon-width"><i class="far fa-user"></i></i></span>About</a> </li>
+
+        <li class="nav-item px-lg-2 dropdown d-menu">
+        <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="d-inline-block d-lg-none icon-width"><i class="far fa-caret-square-down"></i></span>Dropdown
+        <svg  id="arrow" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+        <polyline points="6 9 12 15 18 9"></polyline>
+        </svg>
+        </a>
+        <div class="dropdown-menu shadow-sm sm-menu" aria-labelledby="dropdown01">
+        <a class="dropdown-item" href="#">Action</a>
+        <a class="dropdown-item" href="#">Another action</a>
+        <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+        </li>
+
+        <li class="nav-item px-lg-2"> <a class="nav-link" href="#"><span class="d-inline-block d-lg-none icon-width"><i class="far fa-envelope"></i></span>Contact</a> </li>
+    </ul>
+
+    <ul class="navbar-nav ml-auto mt-3 mt-lg-0">
+        <li class="nav-item"> <a class="nav-link" href="#">
+        <i class="fab fa-twitter"></i><span class="d-lg-none ml-3">Twitter</span>
+        </a> </li>
+        <li class="nav-item"> <a class="nav-link" href="#">
+        <i class="fab fa-facebook"></i><span class="d-lg-none ml-3">Facebook</span>
+        </a> </li>
+        <li class="nav-item"> <a class="nav-link" href="#">
+        <i class="fab fa-instagram"></i><span class="d-lg-none ml-3">Instagram</span>
+        </a> </li>
+        <li class="nav-item"> <a class="nav-link" href="#">
+        <i class="fab fa-linkedin"></i><span class="d-lg-none ml-3">Linkedin</span>
+        </a> </li>
+    </ul>
+    </div>
+    </div>
+</nav>
+
+
+<div class="py-5 text-center text-white col-md-12" id="header" style="background-image:url('images/plainBg.png'); width:100%; heigth:100%;">
+    <div class="py-5 mx-md-0 container col-md-12">
+        <div class="row py-5 md-8 col-md-12 md-flex" >
+            <div class="wrapper py-5 col-md-6">
+                <img src="images/trayk-flip.png" width="70%" alt="">
+            </div>
+            <div class="mx-auto col-md-6">
+                <h1 class="display-4 mb-4 text-start" id="booknow"><b id="easy">Easy and safe to</b>
+                <a href="" class="typewrite display-4 mb-4" data-period="2000" data-type='[ "Book!", "Ride!", "Join!"]'>
+                    <span class="wrap"></span>
+                </a>
+                </h1>
+                <h1 id="with" class="text-start">with TraykPila</h1>
+                <p class="lead mt-4 mb-6 text-start" >TraykPila is a tricycle booking application that helps every Filipino commuter book and rides a safe tricycle to the nearest terminal around the area via mobile phones. </p> 
+                <a href="#" class="btn btn-lg btn-outline-light mx-1">Download here!</a> 
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<!-- Body -->
+<div class="pt-5 text-center text-white col-md-12 border-bottom">
+    <div class=" mx-6 container col-md-12">
+        <div class="row md-8 col-md-12 md-flex" >
+            <div class="mx-auto col-md-6 text-dark mt-5 pt-5">
+                <h1 class="text-start">Are you a tricycle commuter?</h1>
+                <p class="lead text-start">Go and book now in Traykpila to the nearest terminal in your area to get you to your destination. The Tricycle will pick you up at your address in time.</p>
+            </div>
+            <div class="wrapper py-5 col-md-6">
+                <img src="images/commuters.png" width="100%" alt="">
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="text-center text-white col-md-12 border-bottom">
+    <div class=" mx-6 container col-md-12">
+        <div class="row md-8 col-md-12 md-flex" >
+            <div class="wrapper col-md-6">
+                <img src="images/driver.png" width="100%" alt="">
+            </div>
+            <div class="mx-auto col-md-6 text-dark mt-5 pt-5">
+                <h1 class="text-start">How to secure with the drivers?</h1>
+                <p class="lead text-start">All the tricycle drivers go to the background checking before being verified as TraykPila drivers. All their information can be found in their profile.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="text-center text-white col-md-12 border-bottom">
+    <div class=" mx-6 container col-md-12">
+        <div class="row md-8 col-md-12 md-flex" >
+            <div class="mx-auto col-md-12 text-dark mt-5 pt-5">
+                <h1 class="text-center">Do I have to wait until the GrabTrike gets full?</h1>
+                <p class="lead text-center">No need to wait! Once the traypila tricycle drivers found you are onboard then He will pick you up in your location. The tricycle driver will pick you up once you are ready.</p>
+            </div>
+        </div>
+    </div>
+</div>
+<br><br>
+
+
+
+
+<!-- footer -->
+<section class="contact-area py-4" style="background:#e2e2e2;" id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 offset-lg-3">
+                    <div class="contact-content text-center">
+                        <img src="images/traykpilalogo.png" width="200px" version="1.1" alt="">
+                        <p>Don’t forget to share your experience with  #TraykPila on Facebook, Twitter and Instagram! So tag along your friends or family and invite them to book a TraykPila as well.</p>
+                        <a href="#" class="btn btn-md btn-outline-dark mx-1">Download here!</a> 
+                        <p>Download TraykPila to your device.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer>
+        <p class="mb-0 py-3">Copyright &copy; <?php echo date("Y"); ?> <img src="images/traykpilalogo.png" width="200px" alt="logo"> All Rights Reserved.</p>
+    </footer>
+
+
+
+
+
+
+
+
+
+
+
+    <script>
+        var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+        }
+
+        setTimeout(function() {
+        that.tick();
+        }, delta);
+    };
+
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: .15em solid #11914a}";
+        document.body.appendChild(css);
+    };
+    </script>
+</body>
+</html>
