6b61b668a0b5997fbf554c968dbc5ffc62f515e3
johncarlo2020 <77434763+johncarlo2020@users.noreply.github.com>
2022-12-28 20:57:02 +0800

    changes

--- database/migrations/2022_11_03_020221_addroletouser.php
+++ database/migrations/2022_11_03_020221_addroletouser.php
@@ -14,7 +14,7 @@ class Addroletouser extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('role');
+            $table->string('role');
         });
     }
 
