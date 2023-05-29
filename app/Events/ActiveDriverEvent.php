8cf237de7da8cc6e0b26070c3eaeca8deaf98e9f
johncarlo2020 <77434763+johncarlo2020@users.noreply.github.com>
2023-05-29 22:53:23 +0800

    fix active status

--- app/Events/ActiveDriverEvent.php
+++ app/Events/ActiveDriverEvent.php
@@ -10,20 +10,22 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class ActiveDriverEvent
+class ActiveDriverEvent implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
     public $data;
+    public $total;
 
     /**
      * Create a new event instance.
      *
      * @return void
      */
-    public function __construct($data)
+    public function __construct($data,$total)
     {
         $this->data = $data;
+        $this->total = $total;
     }
 
     /**
--- app/Http/Controllers/PusherController.php
+++ app/Http/Controllers/PusherController.php
@@ -21,15 +21,14 @@ class PusherController extends Controller
     public function activeDriver(Request $request)
     {
         $id = $request->input('id');
-
         $user = User::find($id);
-        $user->active = ($user->active === null || $user->active == 0) ? 1 : 0;
+        $user->active = $request->input('status');
         $user->save();
 
         $total = User::where('role',1)->where('active',1)->count();
-        $user->total = $total;
+        
 
-        event(new ActiveDriverEvent($user));
+        event(new ActiveDriverEvent($user,$total));
 
         return response()->json(['message' => $user]);
     }
